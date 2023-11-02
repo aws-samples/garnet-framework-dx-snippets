78051c1a6133a23bd508aab246c960432fe26fb7
Benfattoum <alidiali@gmail.com>
2023-11-02 11:31:57 +0100

    Initial commit

---
+++ test/garnet-dx-examples.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GarnetDxExamples from '../lib/garnet-dx-examples-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/garnet-dx-examples-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GarnetDxExamples.GarnetDxExamplesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
